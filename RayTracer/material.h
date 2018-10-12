5c34ee24c8e3e6eb149c56426e43744554bfa6d7
SweeneyChoi <caitianxin001@163.com>
2018-10-12 17:34:15 +0800

    Update class diagram

--- RayTracer/material.h
+++ RayTracer/material.h
@@ -4,7 +4,7 @@
 
 class Material {
 public:
-	virtual bool scatter(const Ray& r_in, const Hit_record& rec, Vec3& attenuation, Ray&scattered) const = 0;
+	virtual bool scatter(const Ray& r_in, const Hit_record& rec, Vec3& attenuation, Ray& scattered) const = 0;
 };
 #endif // !MATERIAL_H
 
